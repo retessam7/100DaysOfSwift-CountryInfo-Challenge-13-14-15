dde8055174c983d91a3ae7cc96dea183ba60635b
retessam7 <retessam7@gmail.com>
2025-01-24 16:16:16 +0100

    Initial Commit

---
+++ Project131415/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project131415
+//
+//  Created by VII on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
